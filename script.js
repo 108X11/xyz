920606270cf96bfb0ec7fc17046494610ce62c11
Lugh <93958034+Chazz980@users.noreply.github.com>
2022-12-05 10:39:26 +0800

    commit

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function equal(){
     document.form.textview.value = eval(exp)
     }
 }
-function clear(){
+function c(){
     document.form.textview.value = "";
 }
 function back(){
     var exp = document.form.textview.value;
-    var exp = document.form.textview.value = exp.substring(0.exp.length-1);
+    var exp = document.form.textview.value = exp.substring(0,exp.length-1);
 }
